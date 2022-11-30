abd70ba6c0b3a7ba992adadf06ba27595785f940
Vlad <70889664+TokioBoy@users.noreply.github.com>
2022-11-30 18:26:10 +0100

    n

---
+++ Lesson15/help.py
@@ -0,0 +1,15 @@
+import math as mt
+import platform as p
+import random
+
+help(random.randint)
+help(mt.sin)
+#help - gives info about method
+
+print(p.system())
+
+
+area = mt.pi * 3 ** 2
+print(area)
+
+print(sum(range(5)))
